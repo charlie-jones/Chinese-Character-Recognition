62afabf368663397e58cb682350755e0a4068f28
Ryan Chu <plochemisswag@gmail.com>
2019-12-04 14:51:44 -0800

    creates venv in different cmd prompt

--- install-windows.sh
+++ install-windows.sh
@@ -1,7 +1,7 @@
 py -m pip install --user --upgrade pip
-py -m pip install --user Flask
-
 py -m pip install --user virtualenv
 py -m virtualenv env
 
-call env/Scripts/activate
+start ".\env\Scripts\activate"
+
+#pip install --user Flask
