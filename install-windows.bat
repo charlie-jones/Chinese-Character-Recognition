5efcb948991f48a8f8c7908c5468fae55207f960
Ryan Chu <plochemisswag@gmail.com>
2019-12-04 23:50:35 -0800

    don't need &&

--- install-windows.bat
+++ install-windows.bat
@@ -3,7 +3,8 @@ py -m pip install --user virtualenv
 py -m virtualenv env
 
 
-CALL ".\env\Scripts\activate.bat" && pip install Flask
+CALL ".\env\Scripts\activate.bat"
+pip install Flask
 set FLASK_APP=backendMain.py
 set FLASK_ENV=development
 flask run --host=0.0.0.0
