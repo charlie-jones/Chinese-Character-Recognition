0df40e8554e0f39b90e4a580d3b34c2b209dba15
Ryan Chu <plochemisswag@gmail.com>
2019-12-26 14:50:48 -0800

    filter with padding

--- Filter3x3.py
+++ Filter3x3.py
@@ -1,5 +1,4 @@
 from numpy import exp, array, random, dot, sum, zeros, pad
-from PIL import Image
 
 class Filter3x3:
     n_filters = 0
@@ -13,14 +12,17 @@ class Filter3x3:
     Outputs 3d array of transformed images
     '''
     def feedForward(self, imageMatrix): # input image 2d array/matrix
+        imageMatrix = pad(imageMatrix, (1, 1), 'constant') # pad 0s around
         h, w = imageMatrix.shape
-        transformedImage = np.zeros((h - 2, w - 2, self.n_filters))
-        for i in range(h - 2): # iterates all possible 3x3 regions in the image
-            for j in range(w - 2):
+        transformedImage = zeros((self.n_filters, h-2, w-2)) # same dimension as original image matrix
+        for i in range(h-2): # iterates all possible 3x3 regions in the image
+            for j in range(w-2):
                 temp3x3 = imageMatrix[i:(i+3), j:(j+3)] #selects 3x3 area using current indexes
-                transformedImage[i, j] = sum(temp3x3, self.filters, axis=(1,2))
+                for k in range(len(self.filters)):
+                    transformedImage[k, i, j] = sum(temp3x3 * self.filters[k])
         return transformedImage
-
+test = Filter3x3(2)
+print(test.feedForward(array([[11, 12, 5, 2, 3], [15, 6, 10, 9, 3], [10, 8, 12, 5, 3], [12,15,8,6, 3], [12,15,8,6, 3]])))
 
 
 
