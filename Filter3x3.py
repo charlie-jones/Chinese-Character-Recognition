a97769b4c40f13d74d35945f2f6efd3a94fc000f
Ryan Chu <plochemisswag@gmail.com>
2019-12-27 22:15:58 -0800

    sofxmax probablity

--- Filter3x3.py
+++ Filter3x3.py
@@ -1,8 +1,10 @@
 from numpy import exp, array, random, dot, sum, zeros, pad, amax
-
+#128x128
 class Filter3x3:
     n_filters = 0
     filters = []
+    weights = []
+    biases = []
     def __init__(self, n_filters):
         self.n_filters = n_filters
         self.filters = random.randn(n_filters, 3, 3) / 9 # generate 3D array (3x3xn_filters)
@@ -12,6 +14,7 @@ class Filter3x3:
     Outputs 3d array of transformed images
     '''
     def feedForward(self, imageMatrix): # input image 2d array/matrix
+        imageMatrix = (imageMatrix / 255) - 0.5 # make values between -0.5 and 0.5                              #
         imageMatrix = pad(imageMatrix, (1, 1), 'constant') # pad 0s around
         h, w = imageMatrix.shape
         transformedImage = zeros((self.n_filters, h-2, w-2)) # same dimension as original image matrix
@@ -36,12 +39,55 @@ class Filter3x3:
                     tempSel = imageMatrix[k, (i * poolSize):(i * poolSize + poolSize), (j * poolSize):(j * poolSize + poolSize)]
                     transformedImage[k, i, j] = amax(tempSel)
         return transformedImage
-filter = Filter3x3(1)
-test = filter.feedForward(array([[11, 12, 5, 2, 3 , 6], [15, 6, 10, 9, 3, 6], [10, 8, 12, 5, 3, 6], [12,2,9,6, 3, 12], [12,15,8,6,3, 6], [11,10,5,2,3, 4]]))
-print(test)
-test = filter.pool(test, 2)
-print(test)
 
+    '''
+    Calculate the probablity of result
+    '''
+    def softmax(self, n_inputs, n_nodes, input):
+        # generate weights and biases
+        self.weights = random.randn(n_inputs, n_nodes) / n_inputs 
+        n_inputs, n_nodes = self.weights.shape
+        self.biases = zeros(n_nodes)
+        input = input.flatten() # to 1d instead of 3d
+
+        total = dot(input, self.weights) + self.biases
+        ex = exp(total)
+        return ex / sum(ex, axis=0) # softmax function for probablity
+
+    print('MNIST CNN initialized!')
+
+    loss = 0
+    num_correct = 0
+    for i, (im, label) in enumerate(zip(test_images, test_labels)):
+    # Do a forward pass.
+    _, l, acc = forward(im, label)
+    loss += l
+    num_correct += acc
+
+    # Print stats every 100 steps.
+    if i % 100 == 99:
+        print(
+        '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
+        (i + 1, loss / 100, num_correct)
+        )
+        loss = 0
+        num_correct = 0
+
+filter = Filter3x3(2)
+out = filter.feedForward(array([[11, 12, 5, 2, 3 , 6], [15, 6, 10, 9, 3, 6], [10, 8, 12, 5, 3, 6], [12,2,9,6, 3, 12], [12,15,8,6,3, 6], [11,10,5,2,3, 4]]))
+# print(test)
+# print(test.shape)
+out = filter.pool(out, 2)
+# print(test)
+# print(test.shape)
+f, h, w = out.shape
+out = filter.softmax(f * h * w, 6825, out) # array of probabilities
+# print(test)
+# print(test.shape)
+
+
+loss = -np.log(out[label]) # loss of accuracy
+acc = 1 if np.argmax(out) == label else 0 # 1 = correct, 0 = incorrect
 
 
                 
